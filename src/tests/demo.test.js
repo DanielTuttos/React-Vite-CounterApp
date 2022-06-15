4569fc1c617694a02619b5aad01bcc7a077c411d
Daniel Romero AG <tutorialesdanielromero@gmail.com>
2022-06-15 11:41:48 -0500

    added package jest from tests and test demo

---
+++ src/tests/demo.test.js
@@ -0,0 +1,14 @@
+describe('Tests in <DemoComponent />', () => {
+
+    test('Esta prueba no debe de fallar', () => {
+        // 1. inicializacion
+        const message1 = 'hola mundo'
+
+        // 2. estimulo
+        const message2 = message1.trim();
+
+        // 3. observar el comportamiento  esperado
+        expect(message1).toBe(message2);
+
+    })
+});
